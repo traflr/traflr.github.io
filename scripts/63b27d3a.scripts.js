d6da2f9af445b3cbde20b205b26ab2e80d9a2da6
waskito <yg.waskito@gmail.com>
2014-04-07 15:35:26 +0700

    snazzy

---
+++ scripts/63b27d3a.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("traflrgithubioApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("traflrgithubioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
